93aeb1e9528e3acd1aa3aaf28528228abfbbd292
Scelerat99 <56557534+Scelerat99@users.noreply.github.com>
2023-08-01 22:19:24 +0300

    Change request status

--- src/controllers/user.ts
+++ src/controllers/user.ts
@@ -8,7 +8,7 @@ export const loginOne = async (req: Request, res: Response) => {
       const foundUser = await userServices.login(req.body);
       //console.log('found user', foundUser.token);
       res.status(200).send(foundUser);
-    } catch (error) {
+    } catch (error:any) {
       return res.status(500).send(getErrorMessage(error));
     }
 }
@@ -16,14 +16,9 @@ export const loginOne = async (req: Request, res: Response) => {
 export const registerOne =async (req: Request, res: Response) => {
   try {
     const newUser = await userServices.register(req.body);
-    if(newUser instanceof Error){
-      res.status(400).send(newUser)
-    }
-    else{
-      res.status(200).send(newUser);
-    }
+    res.status(200).send(newUser)
     //console.log('found user', foundUser.token);
-  } catch (error) {
+  } catch (error:any) {
     return res.status(500).send(getErrorMessage(error));
   }
 }
